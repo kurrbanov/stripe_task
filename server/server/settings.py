2c39ca02059c7540eb17bc07ea689f7f39da540c
kurrbanov <justnick1312@gmail.com>
2022-05-18 22:44:49 +0300

    integrated docker

--- server/server/settings.py
+++ server/server/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': getenv("POSTGRES_DB"),
+        'USER': getenv("POSTGRES_USER"),
+        'PASSWORD': getenv("POSTGRES_PASSWORD"),
+        'HOST': 'db',
+        'PORT': 5432
     }
 }
 
