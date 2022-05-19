0f78eee97fba7fd8a2a73120b2e982bea73b786b
kurrbanov <justnick1312@gmail.com>
2022-05-19 20:06:08 +0300

    added payments

--- server/server/urls.py
+++ server/server/urls.py
@@ -1,25 +1,15 @@
-"""server URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 
-from stripe_app.views import main_page, item_info
+from stripe_app.views import (
+    main_page, item_info, create_checkout_session, success_page, cancel_page
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main_page, name="main"),
-    path('item/<int:pk>', item_info, name="item")
+    path('item/<int:pk>', item_info, name="item"),
+    path('buy/<int:pk>', create_checkout_session, name="buy"),
+    path('success', success_page, name="success"),
+    path('cancel', cancel_page, name="cancel")
 ]
--- server/stripe_app/views.py
+++ server/stripe_app/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+import os
+import stripe
+
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse
 
 from stripe_app.models import Item
+
+stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
 
 
 def main_page(request):
@@ -13,5 +18,40 @@ def item_info(request, pk):
     try:
         item = Item.objects.get(id=pk)
     except Item.DoesNotExist:
-        return HttpResponse(f"<h1>Item #{pk} not found</h1>")
-    return render(request, 'item_info.html', context={"item": item})
+        return HttpResponse(f"<h1>Item with id={pk} not found</h1>")
+    return render(request, 'item_info.html', context={"item": item, "STRIPE_PUBLIC": os.getenv("STRIPE_PUBLIC_KEY")})
+
+
+def create_checkout_session(request, pk):
+    try:
+        item = Item.objects.get(id=pk)
+    except Item.DoesNotExist:
+        return HttpResponse(f"<h1>Item with id={pk} not found</h1>")
+
+    session = stripe.checkout.Session.create(
+        line_items=[{
+            'price_data': {
+                'currency': 'usd',
+                'product_data': {
+                    'name': item.name,
+                },
+                'unit_amount': item.price,
+            },
+            'quantity': 1,
+        }],
+        mode='payment',
+        success_url='http://localhost:8000/success',
+        cancel_url='http://localhost:8000/cancel',
+    )
+
+    return JsonResponse({
+        'id': session.id
+    })
+
+
+def success_page(request):
+    return render(request, 'success.html')
+
+
+def cancel_page(request):
+    return render(request, 'cancel.html')
